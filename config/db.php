ca64048e5bb80448763e5d4832c60086285f2782
DotNero <71886633+DotNero@users.noreply.github.com>
2022-08-19 23:53:27 +0300

    add

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=keepdatabase',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
